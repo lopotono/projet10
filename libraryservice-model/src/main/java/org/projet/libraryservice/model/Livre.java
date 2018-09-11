8131a4a1e3de8ee985370a0bfdbe4953e2ca2db2
lopotono <30410435+lopotono@users.noreply.github.com>
2018-09-11 10:45:23 +0200

    Add libraryservice-model

---
+++ libraryservice-model/src/main/java/org/projet/libraryservice/model/Livre.java
@@ -0,0 +1,86 @@
+package org.projet.libraryservice.model;
+
+public class Livre {
+
+    private int livreid;
+    private String titre;
+    private String auteur;
+    private int isbn;
+    private String genre;
+    private boolean disponible;
+    private int nbexemplaire;
+    private String editeur;
+    private String image;
+
+    public int getLivreid() {
+        return livreid;
+    }
+
+    public void setLivreid(int livreid) {
+        this.livreid = livreid;
+    }
+
+    public String getTitre() {
+        return titre;
+    }
+
+    public void setTitre(String titre) {
+        this.titre = titre;
+    }
+
+    public String getAuteur() {
+        return auteur;
+    }
+
+    public void setAuteur(String auteur) {
+        this.auteur = auteur;
+    }
+
+    public int getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(int isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public boolean isDisponible() {
+        return disponible;
+    }
+
+    public void setDisponible(boolean disponible) {
+        this.disponible = disponible;
+    }
+
+    public int getNbexemplaire() {
+        return nbexemplaire;
+    }
+
+    public void setNbexemplaire(int nbexemplaire) {
+        this.nbexemplaire = nbexemplaire;
+    }
+
+    public String getEditeur() {
+        return editeur;
+    }
+
+    public void setEditeur(String editeur) {
+        this.editeur = editeur;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+}
