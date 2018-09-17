79e4868daae1a5f30fd528bb131bc18bb722f0b1
lopotono <30410435+lopotono@users.noreply.github.com>
2018-09-17 18:33:02 +0200

    Add User.java

---
+++ libraryservice-model/src/main/java/org/projet/libraryservice/model/User.java
@@ -0,0 +1,69 @@
+package org.projet.libraryservice.model;
+
+public class User {
+	
+	private int id;
+	private String name;
+	private String prenom;
+	private String adresse;
+	private int codepostal;
+	private String ville;
+	private String password;
+	private String mail;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getPrenom() {
+		return prenom;
+	}
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	
+	public String getAdresse() {
+		return adresse;
+	}
+	public void setAdresse(String adresse) {
+		this.adresse = adresse;
+	}
+	
+	public int getCodepostal() {
+		return codepostal;
+	}
+	public void setCodepostal(int codepostal) {
+		this.codepostal = codepostal;
+	}
+	
+	public String getVille() {
+		return ville;
+	}
+	public void setVille(String ville) {
+		this.ville = ville;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public String getMail() {
+		return mail;
+	}
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+}
