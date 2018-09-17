d01ebe295ed490b85423e6bac692f446acbc8eee
lopotono <30410435+lopotono@users.noreply.github.com>
2018-09-17 18:25:26 +0200

    Add UserManagerImpl.java

---
+++ libraryservice-business/src/main/java/org/projet/libraryservice/business/impl/UserManagerImpl.java
@@ -0,0 +1,11 @@
+package org.projet.libraryservice.business.impl;
+
+import org.projet.libraryservice.business.contract.UserManager;
+import org.projet.libraryservice.model.User;
+
+public class UserManagerImpl extends AbstractManager implements UserManager {
+
+	public User getUser(String name, String password) {
+		return null;
+	}
+}
