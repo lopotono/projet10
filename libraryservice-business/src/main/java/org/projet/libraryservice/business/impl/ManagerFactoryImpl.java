f35f074fa7ffbfd77fd5ac2683d7e2280c541e85
lopotono <30410435+lopotono@users.noreply.github.com>
2018-09-17 18:24:25 +0200

    Update ManagerFactoryImpl.java

--- libraryservice-business/src/main/java/org/projet/libraryservice/business/impl/ManagerFactoryImpl.java
+++ libraryservice-business/src/main/java/org/projet/libraryservice/business/impl/ManagerFactoryImpl.java
@@ -2,11 +2,26 @@ package org.projet.libraryservice.business.impl;
 
 import org.projet.libraryservice.business.contract.LivreManager;
 import org.projet.libraryservice.business.contract.ManagerFactory;
+import org.projet.libraryservice.business.contract.UserManager;
 
 public class ManagerFactoryImpl implements ManagerFactory {
 
     private LivreManager livreManager;
+    private UserManager userManager;
+    
     public LivreManager getLivreManager() {
         return this.livreManager;
     }
+
+    public void setLivreManager(LivreManager livreManager) {
+        this.livreManager = livreManager;
+    }
+
+	public UserManager getUserManager() {
+		return this.userManager;
+	}
+	
+	public void setUserManager(UserManager userManager) {
+		this.userManager = userManager;
+	}
 }
