8a9ac1e2084e7b89179b62103e35ef86a96ff328
lopotono <30410435+lopotono@users.noreply.github.com>
2018-09-17 18:30:39 +0200

    Add UserRowMapper.java

---
+++ libraryservice-consumer/src/main/java/org/projet/libraryservice/consumer/impl/rowmapper/UserRowMapper.java
@@ -0,0 +1,24 @@
+package org.projet.libraryservice.consumer.impl.rowmapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.springframework.jdbc.core.RowMapper;
+
+import org.projet.libraryservice.model.User;
+
+public class UserRowMapper implements RowMapper<User> {
+	
+	public User mapRow(ResultSet pRS, int pRowNum) throws SQLException {
+		User vUser = new User();
+		vUser.setId(pRS.getInt("id_user"));
+		vUser.setName(pRS.getString("nom"));
+		vUser.setPrenom(pRS.getString("prenom"));
+		vUser.setAdresse(pRS.getString("adresse"));
+		vUser.setCodepostal(pRS.getInt("code_postal"));
+		vUser.setVille(pRS.getString("ville"));
+		vUser.setPassword(pRS.getString("mot_de_passe"));
+		vUser.setMail(pRS.getString("mail"));
+		return vUser;		
+	}
+}
