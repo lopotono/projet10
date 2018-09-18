fd2bdab20af34941512fdbfcb095b01556be72dc
lopotono <30410435+lopotono@users.noreply.github.com>
2018-09-18 15:21:30 +0200

    Update LivreDAOImpl.java

--- libraryservice-consumer/src/main/java/org/projet/libraryservice/consumer/impl/LivreDAOImpl.java
+++ libraryservice-consumer/src/main/java/org/projet/libraryservice/consumer/impl/LivreDAOImpl.java
@@ -7,30 +7,41 @@ import org.projet.libraryservice.model.Livre;
 import java.util.List;
 
 public class LivreDAOImpl extends AbstractDaoImpl implements LivreDAO {
-	
+
 	public LivreDAOImpl() {
-		
+
 	}
 
 	public List<Livre> getLivres() {
 
-        String vSQL = "SELECT * FROM livre ORDER BY titre";
+		String vSQL = "SELECT * FROM livre ORDER BY titre";
 
-        LivreRowMapper vRowMapper = new LivreRowMapper();
-
-        List<Livre> vListLivres = getJdbcTemplate().query(vSQL, vRowMapper);
-
-        return vListLivres;
-    }
-
-	public List<Livre> getSearchLivre(String titre) {
-		
-		String vSQL = "SELECT * FROM livre WHERE titre LIKE '%" + titre + "%'";
-		
 		LivreRowMapper vRowMapper = new LivreRowMapper();
 
-        List<Livre> livres = getJdbcTemplate().query(vSQL, vRowMapper);
-	
+		List<Livre> vListLivres = getJdbcTemplate().query(vSQL, vRowMapper);
+
+		return vListLivres;
+	}
+
+	public List<Livre> getSearchLivre(String titre) {
+
+		String vSQL = "SELECT * FROM livre WHERE titre LIKE '%" + titre + "%'";
+
+		LivreRowMapper vRowMapper = new LivreRowMapper();
+
+		List<Livre> livres = getJdbcTemplate().query(vSQL, vRowMapper);
+
 		return livres;
+	}
+
+	public List<Livre> getLivresDispo(boolean livredispo) {
+
+		String vSQL = "SELECT * FROM livre WHERE disponible=" + livredispo;
+
+		LivreRowMapper vRowMapper = new LivreRowMapper();
+
+		List<Livre> vListLivres = getJdbcTemplate().query(vSQL, vRowMapper);
+
+		return vListLivres;
 	}
 }
