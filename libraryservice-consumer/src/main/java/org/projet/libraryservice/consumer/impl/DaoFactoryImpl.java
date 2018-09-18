b6ee384d847d3d0f2ed75d416c0a08236b513812
lopotono <30410435+lopotono@users.noreply.github.com>
2018-09-18 15:20:54 +0200

    Update DaoFactoryImpl.java

--- libraryservice-consumer/src/main/java/org/projet/libraryservice/consumer/impl/DaoFactoryImpl.java
+++ libraryservice-consumer/src/main/java/org/projet/libraryservice/consumer/impl/DaoFactoryImpl.java
@@ -2,12 +2,14 @@ package org.projet.libraryservice.consumer.impl;
 
 import org.projet.libraryservice.consumer.contract.DaoFactory;
 import org.projet.libraryservice.consumer.contract.LivreDAO;
+import org.projet.libraryservice.consumer.contract.PretDAO;
 import org.projet.libraryservice.consumer.contract.UserDAO;
 
 public class DaoFactoryImpl implements DaoFactory {
 
     private LivreDAO livreDao;
     private UserDAO userDao;
+    private PretDAO pretDao;
 
     public LivreDAO getLivreDao() {
         return livreDao;
@@ -23,5 +25,13 @@ public class DaoFactoryImpl implements DaoFactory {
 	
 	public void setUserDao(UserDAO userDao) {
 		this.userDao = userDao;
+	}
+
+	public PretDAO getPretDao() {
+		return pretDao;
+	}
+
+	public void setPretDao(PretDAO pretDao) {
+		this.pretDao = pretDao;
 	}
 }
