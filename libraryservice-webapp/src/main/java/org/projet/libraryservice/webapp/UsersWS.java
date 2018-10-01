a55b1a6b6983cd5dcd5e4fa797ba5b69edba1824
lopotono <30410435+lopotono@users.noreply.github.com>
2018-10-01 17:12:12 +0200

    Add UsersWS.java

---
+++ libraryservice-webapp/src/main/java/org/projet/libraryservice/webapp/UsersWS.java
@@ -0,0 +1,25 @@
+package org.projet.libraryservice.webapp;
+
+import java.util.List;
+
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+
+import org.projet.libraryservice.model.User;
+
+@WebService(serviceName = "UsersWS")
+public class UsersWS extends AbstractWebapp {
+		
+	@WebMethod
+	public List<User> doList() {
+		return getManagerFactory().getUserManager().getUsers();
+	}
+	
+	@WebMethod
+	public User doLogin(String name, String password) {
+		
+		User vUser = getManagerFactory().getUserManager().getUser(name, password);
+		
+		return vUser;		
+	}
+}
