29300060bfd686c6fa1fc1072a00372ff15055a5
lopotono <30410435+lopotono@users.noreply.github.com>
2018-10-06 15:45:39 +0200

    Update library-consumer

---
+++ library-consumer/src/main/java/org/projet/library/consumer/impl/DaoFactoryImpl.java
@@ -0,0 +1,34 @@
+package org.projet.library.consumer.impl;
+
+import org.projet.library.consumer.contract.DaoFactory;
+import org.projet.library.consumer.contract.LivreDAO;
+import org.projet.library.consumer.contract.PretDAO;
+import org.projet.library.consumer.contract.UserDAO;
+
+public class DaoFactoryImpl implements DaoFactory {
+	
+	private LivreDAO livreDao;
+	private PretDAO pretDao;
+	private UserDAO userDao;
+	
+	public LivreDAO getLivreDao() {
+		return livreDao;
+	}
+	public void setLivreDao(LivreDAO livreDao) {
+		this.livreDao = livreDao;
+	}
+	
+	public PretDAO getPretDao() {
+		return pretDao;
+	}
+	public void setPretDao(PretDAO pretDao) {
+		this.pretDao = pretDao;
+	}
+	
+	public UserDAO getUserDao() {
+		return userDao;
+	}
+	public void setUserDao(UserDAO userDao) {
+		this.userDao = userDao;
+	}
+}
--- library-consumer/src/main/java/org/projet/library/consumer/impl/LivreDAOImpl.java
+++ library-consumer/src/main/java/org/projet/library/consumer/impl/LivreDAOImpl.java
@@ -8,7 +8,7 @@ import org.projet.library.model.livres.LivresWS;
 import org.projet.library.model.livres.LivresWS_Service;
 
 public class LivreDAOImpl implements LivreDAO {
-
+	
 	public List<Livre> getLivres() {
 
 		LivresWS stub = new LivresWS_Service().getLivresWSPort();
