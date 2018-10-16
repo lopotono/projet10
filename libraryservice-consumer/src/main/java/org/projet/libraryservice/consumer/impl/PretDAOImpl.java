d0c6b26da4048c041252347f15d7b01519343e2c
lopotono <30410435+lopotono@users.noreply.github.com>
2018-10-16 07:21:16 +0200

    Update PretDAOImpl.java

--- libraryservice-consumer/src/main/java/org/projet/libraryservice/consumer/impl/PretDAOImpl.java
+++ libraryservice-consumer/src/main/java/org/projet/libraryservice/consumer/impl/PretDAOImpl.java
@@ -1,27 +1,52 @@
 package org.projet.libraryservice.consumer.impl;
 
 import java.sql.Types;
-import java.util.Calendar;
-import java.util.Date;
+import java.util.List;
 
 import org.projet.libraryservice.consumer.contract.PretDAO;
+import org.projet.libraryservice.consumer.impl.rowmapper.PretRowMapper;
+import org.projet.libraryservice.model.Pret;
+import org.projet.libraryservice.model.User;
 import org.springframework.dao.DuplicateKeyException;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 
 public class PretDAOImpl extends AbstractDaoImpl implements PretDAO {
 
-	public void SavePret(Calendar datedebut, Calendar datefin, String etat, Date dateprolongation, String userid, String livreid) {
+	
+	public List<Pret> getListPret() {
 
-		String vSQL = "INSERT INTO pret (date_debut, date_fin, etat, dateprolongation, id_user, id_livre) VALUES (:date_debut,:date_fin,:etat,:dateprolongation,:id_user,:id_livre)";
+		String vSQL = "SELECT * FROM pret";
+
+		PretRowMapper vRowMapper = new PretRowMapper();
+
+		List<Pret> vListPret = getJdbcTemplate().query(vSQL, vRowMapper);
+
+		return vListPret;
+	}
+
+	public List<Pret> getPretByUser(User vUser) {
+
+		String vSQL = "SELECT * FROM pret WHERE id_user=" + vUser.getId();
+
+		PretRowMapper vRowMapper = new PretRowMapper();
+
+		List<Pret> pret = getJdbcTemplate().query(vSQL, vRowMapper);
+
+		return pret;
+	}
+
+	public void update(Pret pret) {
+
+		String vSQL = "UPDATE pret SET etat= :etat, datedebut= :datedebut, datefin= :datefin, dateprolongation= :dateprolongation, id_livre= :id_livre WHERE id_pret = :id_pret";
 
 		MapSqlParameterSource vParams = new MapSqlParameterSource();
-		vParams.addValue("date_debut", datedebut, Types.DATE);
-		vParams.addValue("date_fin", datefin, Types.DATE);
-		vParams.addValue("etat", etat, Types.VARCHAR);
-		vParams.addValue("dateprolongation", dateprolongation, Types.DATE);
-		vParams.addValue("id_user", userid, Types.INTEGER);
-		vParams.addValue("id_livre", livreid, Types.INTEGER);
+		vParams.addValue("etat", pret.getEtat(), Types.VARCHAR);
+		vParams.addValue("id_pret", pret.getId(), Types.INTEGER);
+		vParams.addValue("datedebut", pret.getDatedebut(), Types.DATE);
+		vParams.addValue("datefin", pret.getDatefin(), Types.DATE);
+		vParams.addValue("dateprolongation", pret.getDateprolongation(), Types.DATE);
+		vParams.addValue("id_livre", pret.getTitre(), Types.INTEGER);
 
 		NamedParameterJdbcTemplate vJdbcTemplate = new NamedParameterJdbcTemplate(getDataSource());
 
