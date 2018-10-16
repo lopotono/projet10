9ca63eb5bc10cb2d80074e310c4079ea37a321dc
lopotono <30410435+lopotono@users.noreply.github.com>
2018-10-16 07:19:21 +0200

    Add PretManagerImpl.java

---
+++ libraryservice-business/src/main/java/org/projet/libraryservice/business/impl/PretManagerImpl.java
@@ -0,0 +1,30 @@
+package org.projet.libraryservice.business.impl;
+
+import java.util.Iterator;
+import java.util.List;
+
+import org.projet.libraryservice.business.contract.PretManager;
+import org.projet.libraryservice.model.Livre;
+import org.projet.libraryservice.model.Pret;
+import org.projet.libraryservice.model.User;
+
+public class PretManagerImpl extends AbstractManager implements PretManager {
+
+	public List<Pret> getPretByUser(User vUser) {
+		List<Pret> list = getDaoFactory().getPretDao().getPretByUser(vUser);
+		for (Iterator<Pret> iterator = list.iterator(); iterator.hasNext();) {
+			Pret pret = (Pret) iterator.next();
+			Livre livre = getDaoFactory().getLivreDao().getLivre(pret.getId());
+			pret.setTitre(livre);
+		}
+		return list;
+	}
+
+	public void update(Pret pret) {
+		getDaoFactory().getPretDao().update(pret);		
+	}
+
+	public List<Pret> getListPret() {
+		return getDaoFactory().getPretDao().getListPret();
+	}
+}
