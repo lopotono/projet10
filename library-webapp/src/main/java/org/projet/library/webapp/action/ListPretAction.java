7917cfc450c5648a3867212e283b5f6610e5c52b
lopotono <30410435+lopotono@users.noreply.github.com>
2018-10-29 10:52:59 +0100

    Update ListPretAction.java

--- library-webapp/src/main/java/org/projet/library/webapp/action/ListPretAction.java
+++ library-webapp/src/main/java/org/projet/library/webapp/action/ListPretAction.java
@@ -1,9 +1,12 @@
 package org.projet.library.webapp.action;
 
 import java.util.List;
+import java.util.Map;
 
 import org.projet.library.model.prets.Pret;
 import org.projet.library.model.user.User;
+
+import com.opensymphony.xwork2.ActionSupport;
 
 public class ListPretAction extends AbstractAction {
 
@@ -11,22 +14,49 @@ public class ListPretAction extends AbstractAction {
 	 * 
 	 */
 	private static final long serialVersionUID = 4522159096673660852L;
-
-	private List<Pret> listPret;
-
 	
-	public List<Pret> getListPret() {
-		return listPret;
+	private List<Pret> listPrets;
+	private Pret pret;
+	private Map<String, Object> session;
+	private User vUser;
+
+	public List<Pret> getListPrets() {
+		return listPrets;
+	}
+
+	public void setListPrets(List<Pret> listPrets) {
+		this.listPrets = listPrets;
 	}
 	
-	public void setListPret(List<Pret> listPret) {
-		this.listPret = listPret;
+	public Pret getPret() {
+		return pret;
+	}
+
+	public void setPret(Pret pret) {
+		this.pret = pret;
 	}
 	
+	public Map<String, Object> getSession() {
+		return session;
+	}
 	
+	public void setSession(Map<String, Object> session) {
+		this.session = session;
+	}
+	
+	public User getvUser() {
+		return vUser;
+	}
+	
+	public void setvUser(User vUser) {
+		this.vUser = vUser;
+	}
+										
 	public String listPret() {
-		return null;
-		//User vUser = getManagerFactory().getUserManager().
-		//listPret = getManagerFactory().getPretManager().getPrets(name);
+		//listPrets = getManagerFactory().getPretManager().getListPret();
+		//User vUser = (User) this.session.get("user");
+		listPrets = getManagerFactory().getPretManager().getPretByUser(vUser);
+	
+		return ActionSupport.SUCCESS;
 	}
 }
