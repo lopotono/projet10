aee045960adf466148cce80d249fe359f8aa2660
lopotono <30410435+lopotono@users.noreply.github.com>
2018-11-12 18:02:08 +0100

    Update LivreDAOImpl.java

--- libraryservice-consumer/src/main/java/org/projet/libraryservice/consumer/impl/LivreDAOImpl.java
+++ libraryservice-consumer/src/main/java/org/projet/libraryservice/consumer/impl/LivreDAOImpl.java
@@ -3,7 +3,11 @@ package org.projet.libraryservice.consumer.impl;
 import org.projet.libraryservice.consumer.contract.LivreDAO;
 import org.projet.libraryservice.consumer.impl.rowmapper.LivreRowMapper;
 import org.projet.libraryservice.model.Livre;
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 
+import java.sql.Types;
 import java.util.List;
 
 public class LivreDAOImpl extends AbstractDaoImpl implements LivreDAO {
@@ -25,7 +29,7 @@ public class LivreDAOImpl extends AbstractDaoImpl implements LivreDAO {
 
 	public List<Livre> getSearchLivre(String titre) {
 
-		String vSQL = "SELECT * FROM livre WHERE titre LIKE '%" + titre + "%'";
+		String vSQL = "SELECT * FROM livre WHERE LOWER(titre) LIKE '%" + titre + "%'";
 
 		LivreRowMapper vRowMapper = new LivreRowMapper();
 
@@ -43,5 +47,32 @@ public class LivreDAOImpl extends AbstractDaoImpl implements LivreDAO {
 		List<Livre> vListLivres = getJdbcTemplate().query(vSQL, vRowMapper);
 
 		return vListLivres;
+	}
+
+	public Livre getLivre(int id) {
+
+		String vSQL = "SELECT * FROM livre WHERE id_livre=" + id;
+		
+		LivreRowMapper vRowMapper = new LivreRowMapper();
+
+		List<Livre> vListLivres = getJdbcTemplate().query(vSQL, vRowMapper);
+		
+		return vListLivres.get(0);
+	}
+
+	public void update(Livre livre) {
+		
+		String vSQL = "UPDATE livre SET nbexemplaire= nombre_exemplaire";
+		
+		MapSqlParameterSource vParams = new MapSqlParameterSource();
+		vParams.addValue("nombre_exemplaire", livre.getNbexemplaire(), Types.INTEGER);
+		
+		NamedParameterJdbcTemplate vJdbcTemplate = new NamedParameterJdbcTemplate(getDataSource());
+		
+		try {
+			vJdbcTemplate.update(vSQL, vParams);
+		} catch (DuplicateKeyException vEx) {
+
+		}		
 	}
 }
