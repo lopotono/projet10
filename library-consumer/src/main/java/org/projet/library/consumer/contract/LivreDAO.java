228cdc3571506717b70717ad59cef6357dcb1da7
lopotono <30410435+lopotono@users.noreply.github.com>
2018-11-12 17:52:59 +0100

    Update LivreDAO.java

--- library-consumer/src/main/java/org/projet/library/consumer/contract/LivreDAO.java
+++ library-consumer/src/main/java/org/projet/library/consumer/contract/LivreDAO.java
@@ -4,10 +4,13 @@ import java.util.List;
 
 import org.projet.library.model.livres.Livre;
 
-
 public interface LivreDAO {
 
 	List<Livre> getLivres();
 	
 	List<Livre> getSearch(String titre);
+	
+	void update(Livre livre);
+	
+	Livre getLivre(int id);
 }
