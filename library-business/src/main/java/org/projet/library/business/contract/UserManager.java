8369d4f65834af8019e4cb7beb12408776010132
lopotono <30410435+lopotono@users.noreply.github.com>
2018-11-12 17:52:10 +0100

    Update UserManager.java

--- library-business/src/main/java/org/projet/library/business/contract/UserManager.java
+++ library-business/src/main/java/org/projet/library/business/contract/UserManager.java
@@ -1,8 +1,10 @@
 package org.projet.library.business.contract;
 
-import org.projet.library.model.user.User;
+import org.projet.library.model.users.User;
 
 public interface UserManager {
 	
 	User getUser(String name, String password);
+
+	User getUser(int id);
 }
