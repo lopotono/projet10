810ccfddec3a72474397105e8c62779cad827eae
lopotono <30410435+lopotono@users.noreply.github.com>
2018-11-12 18:15:57 +0100

    Update PretRowMapper.java

--- libraryservice-consumer/src/main/java/org/projet/libraryservice/consumer/impl/rowmapper/PretRowMapper.java
+++ libraryservice-consumer/src/main/java/org/projet/libraryservice/consumer/impl/rowmapper/PretRowMapper.java
@@ -12,14 +12,18 @@ public class PretRowMapper implements RowMapper<Pret> {
 	public Pret mapRow(ResultSet pRS, int pRowNum) throws SQLException {
 		Pret vPret = new Pret();
 		vPret.setId(pRS.getInt("id_pret"));
+		vPret.setDatedebut(pRS.getDate("date_debut"));
 		Calendar cal = Calendar.getInstance();
-		cal.setTime(pRS.getDate("date_debut"));
-		vPret.setDatedebut(cal);
+		cal.setTime(pRS.getDate("date_fin"));
+		vPret.setDatefin(cal);
+		vPret.setEtat(pRS.getString("etat"));
+		if (pRS.getDate("dateprolongation") != null) {
 		Calendar cal1 = Calendar.getInstance();
-		cal1.setTime(pRS.getDate("date_fin"));
-		vPret.setDatefin(cal1);
-		
-		return null;
+		cal1.setTime(pRS.getDate("dateprolongation"));
+		vPret.setDateprolongation(cal1);
+		vPret.setId_livre(pRS.getInt("id_livre"));
+		vPret.setId_user(pRS.getInt("id_utilisateur"));
+		}
+		return vPret;
 	}
-
 }
