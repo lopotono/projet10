d770bec827da243d454f7a970ecd4e78ef0e01ea
lopotono <30410435+lopotono@users.noreply.github.com>
2018-11-12 17:58:57 +0100

    Update UserManagerImpl.java

--- libraryservice-business/src/main/java/org/projet/libraryservice/business/impl/UserManagerImpl.java
+++ libraryservice-business/src/main/java/org/projet/libraryservice/business/impl/UserManagerImpl.java
@@ -1,11 +1,25 @@
 package org.projet.libraryservice.business.impl;
 
+import java.util.List;
+
 import org.projet.libraryservice.business.contract.UserManager;
 import org.projet.libraryservice.model.User;
 
 public class UserManagerImpl extends AbstractManager implements UserManager {
 
 	public User getUser(String name, String password) {
-		return null;
+		return getDaoFactory().getUserDao().getUser(name, password);
+	}
+
+	public User getUser(int id) {
+		return getDaoFactory().getUserDao().getUser(id);
+	}
+
+	public List<User> getUsers() {
+		return getDaoFactory().getUserDao().getUsers();
+	}
+
+	public User getName(String name) {
+		return getDaoFactory().getUserDao().getName(name);
 	}
 }
