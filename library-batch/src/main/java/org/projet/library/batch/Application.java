3e882752c6173b7d2aac8e87b5d3a42c5a9d9e00
lopotono <30410435+lopotono@users.noreply.github.com>
2018-11-26 17:05:59 +0100

    Update Application.java

--- library-batch/src/main/java/org/projet/library/batch/Application.java
+++ library-batch/src/main/java/org/projet/library/batch/Application.java
@@ -1,20 +1,46 @@
 package org.projet.library.batch;
 
+import java.util.List;
+
+import org.projet.library.business.contract.ManagerFactory;
+import org.projet.library.model.livres.Livre;
+import org.projet.library.model.prets.Pret;
+import org.projet.library.model.users.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ImportResource;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableAutoConfiguration(exclude = { DataSourceAutoConfiguration.class })
+@ImportResource({"classpath:businessContext.xml","classpath:consumerContext.xml"})
 public class Application {
-		
+
+	@Autowired
+	private MailSender ms;
+
+	@Autowired
+	private ManagerFactory mf;
+
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(Application.class, args);
 	}
-	
-	@Scheduled(cron = "01 00 15 12 *")
+
+	@Scheduled(cron = "0/15 * * * * ?")
 	public void schedule() {
-		
+		System.out.println("test");
+		List<Pret> list = mf.getPretManager().getPretLate();
+		for (Pret pret : list) {
+			User user = mf.getUserManager().getUser(pret.getIdUser());
+			Livre livre = mf.getLivreManager().getLivre(pret.getIdLivre());
+			String body = "Le livre " + livre.getTitre() + " doit être rendu au plus vite !";
+			//ms.sendMail("terragef@gmail.com", user.getMail(), "Pret en retard", body);
+			System.out.println(body);
+		}
 	}
 }
