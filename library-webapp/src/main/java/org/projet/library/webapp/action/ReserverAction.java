bfdea3dfe6017b337baa8566c147a2c16fae9a2d
lopotono <terrage.frederic@orange.fr>
2019-04-17 17:30:39 +0200

    Update ReserverAction.java

--- library-webapp/src/main/java/org/projet/library/webapp/action/ReserverAction.java
+++ library-webapp/src/main/java/org/projet/library/webapp/action/ReserverAction.java
@@ -1,10 +1,11 @@
 package org.projet.library.webapp.action;
 
 import java.util.Date;
+import java.util.List;
 import java.util.Map;
 
 import org.apache.struts2.interceptor.SessionAware;
-import org.projet.library.model.livres.Livre;
+import org.projet.library.model.reservations.Livre;
 import org.projet.library.model.reservations.Reservation;
 import org.projet.library.model.users.User;
 
@@ -16,26 +17,25 @@ public class ReserverAction extends AbstractAction implements SessionAware {
 
 	private int id;
 	private Map<String, Object> session;
-	private Date datereservation;
-	private String etat;
-	private int iduser;
-	private int idlivre;
 	private int livreid;
-	
+	private User vUser;
+	private List<Reservation> listReservation;
+	private Livre livre;
+
 	public int getId() {
 		return id;
 	}
-	
+
 	public void setId(int id) {
 		this.id = id;
 	}
-		
+
 	@Override
 	public void setSession(Map<String, Object> session) {
 		this.session = session;
-		
+
 	}
-	
+
 	public int getLivreid() {
 		return livreid;
 	}
@@ -44,31 +44,78 @@ public class ReserverAction extends AbstractAction implements SessionAware {
 		this.livreid = livreid;
 	}
 
+	public User getvUser() {
+		return vUser;
+	}
+
+	public void setvUser(User vUser) {
+		this.vUser = vUser;
+	}
+
+	public List<Reservation> getListReservation() {
+		return listReservation;
+	}
+
+	public void setListReservation(List<Reservation> listReservation) {
+		this.listReservation = listReservation;
+	}
+
+	public Livre getLivre() {
+		return livre;
+	}
+
+	public void setLivre(Livre livre) {
+		this.livre = livre;
+	}
+
 	public String execute() {
-		
+
 		String vResult = ActionSupport.INPUT;
-		
+
 		// Récupérer id du livre
-		Livre livre = getManagerFactory().getLivreManager().getLivre(id);
+		org.projet.library.model.livres.Livre livre = getManagerFactory().getLivreManager().getLivre(livreid);
+		Livre livre2 = transforme(livre);
 		Reservation reservation = new Reservation();
-		
 		// Récupérer la date du jour (datereservation)
 		Date dateJour = new Date();
 		// Insérer état à "réservé"
 		reservation.setEtat("réservé");
 		// Insérer datereservation = date du jour
 		reservation.setDatereservation(dateJour);
-		// Récupérer id livre et insérer id du livre 
+		// Récupérer id livre et insérer id du livre
 		reservation.setIdLivre(livre.getLivreid());
-		reservation.setLivre(livre);
+		reservation.setLivre(livre2);
 
-		User user = (User) this.session.get("user");
+		User vUser = (User) this.session.get("user");
+		//listReservation = getManagerFactory().getReservationManager().listReservationByUser(vUser);
 		// Récupérer id utilisateur
-		reservation.setIdUser(user.getId());
-		// Insertion des données (état, date du jour) dans la table réservation
-		getManagerFactory().getReservationManager().insertReservation(datereservation, etat, iduser, idlivre);
+		reservation.setIdUser(vUser.getId());
+		listReservation = getManagerFactory().getReservationManager().getListReservation();
+		// Insertion des données (état, date du jour, id livre et id user) dans la table
+		// réservation
+		getManagerFactory().getReservationManager().insertReservation(reservation);
+
+		if (reservation.getEtat() == "réservé") {
+			addActionMessage("Vous avez réservé le livre : " + livre.getTitre() + " de " + livre.getAuteur());
+		} 
+
 		return vResult;
+
 	}
-	
+
+	private Livre transforme(org.projet.library.model.livres.Livre livre) {
+
+		Livre livre2 = new Livre();
+		livre2.setLivreid(livre.getLivreid());
+		livre2.setTitre(livre.getTitre());
+		livre2.setAuteur(livre.getAuteur());
+		livre2.setDisponible(livre.isDisponible());
+		livre2.setEditeur(livre.getEditeur());
+		livre2.setGenre(livre.getGenre());
+		livre2.setIsbn(livre.getIsbn());
+		livre2.setNbexemplaire(livre.getNbexemplaire());
+		livre2.setPersonnemax(livre.getPersonnemax());
+		return livre2;
+	}
 }
 
