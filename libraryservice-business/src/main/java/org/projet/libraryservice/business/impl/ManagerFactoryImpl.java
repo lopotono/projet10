0e61e460e14fd3f212fc8bfbca65cf07af110a86
lopotono <30410435+lopotono@users.noreply.github.com>
2019-04-22 18:31:01 +0200

    Update ManagerFactoryImpl.java

--- libraryservice-business/src/main/java/org/projet/libraryservice/business/impl/ManagerFactoryImpl.java
+++ libraryservice-business/src/main/java/org/projet/libraryservice/business/impl/ManagerFactoryImpl.java
@@ -2,12 +2,16 @@ package org.projet.libraryservice.business.impl;
 
 import org.projet.libraryservice.business.contract.LivreManager;
 import org.projet.libraryservice.business.contract.ManagerFactory;
+import org.projet.libraryservice.business.contract.PretManager;
+import org.projet.libraryservice.business.contract.ReservationManager;
 import org.projet.libraryservice.business.contract.UserManager;
 
 public class ManagerFactoryImpl implements ManagerFactory {
 
     private LivreManager livreManager;
     private UserManager userManager;
+    private PretManager pretManager;
+    private ReservationManager reservationManager;
     
     public LivreManager getLivreManager() {
         return this.livreManager;
@@ -23,5 +27,22 @@ public class ManagerFactoryImpl implements ManagerFactory {
 	
 	public void setUserManager(UserManager userManager) {
 		this.userManager = userManager;
+	}
+
+	public PretManager getPretManager() {
+		return pretManager;
+	}
+
+	public void setPretManager(PretManager pretManager) {
+		this.pretManager = pretManager;
+	}
+
+	@Override
+	public ReservationManager getReservationManager() {
+		return this.reservationManager;
+	}
+
+	public void setReservationManager(ReservationManager reservationManager) {
+		this.reservationManager = reservationManager;
 	}
 }
