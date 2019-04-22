539faf0336e341b5ed11321afa41d11727ad5041
lopotono <30410435+lopotono@users.noreply.github.com>
2019-04-22 18:27:11 +0200

    Update ReservationWS.java

--- libraryservice-webapp/src/main/java/org/projet/libraryservice/webapp/ReservationWS.java
+++ libraryservice-webapp/src/main/java/org/projet/libraryservice/webapp/ReservationWS.java
@@ -6,23 +6,43 @@ import javax.jws.WebMethod;
 import javax.jws.WebService;
 
 import org.projet.libraryservice.model.Reservation;
-import org.projet.libraryservice.model.User;
 
 @WebService(serviceName = "ReservationWS")
 public class ReservationWS extends AbstractWebapp {
-	
-	@WebMethod
-	public Reservation getReservationByPret(int id) {
 		
-		return getManagerFactory().getReservationManager().getReservationByPret(id);
+	@WebMethod
+	public List<Reservation> listReservationByUserId(int id) {
+		
+		return getManagerFactory().getReservationManager().listReservationByUserId(id);
 	}
 	
 	@WebMethod
-	public List<Reservation> getReservationByUser(User vUser) {
+	public void insertReservation(Reservation reservation) {
 		
-		return getManagerFactory().getReservationManager().getReservationByUser(vUser);
+		getManagerFactory().getReservationManager().insertReservation(reservation);
 	}
 	
+	@WebMethod
+	public List<Reservation> getListReservation() {
+		
+		return getManagerFactory().getReservationManager().getListReservation();
+	}
 	
-
+	@WebMethod
+	public Reservation getReservation(int id) {
+		
+		return getManagerFactory().getReservationManager().getReservation(id);
+	}
+	
+	@WebMethod
+	public void deleteReservation(Reservation reservation) {
+		
+		getManagerFactory().getReservationManager().deleteReservation(reservation);
+	}
+	
+	@WebMethod
+	public List<Reservation> getPosition(int position) {
+		
+		return getManagerFactory().getReservationManager().getPosition(position);		
+	}	
 }
