024ac464b6035f0a98c1b8264d73154e2a55b717
lopotono <30410435+lopotono@users.noreply.github.com>
2019-04-22 18:22:59 +0200

    Add ReservationDAOImpl.java

---
+++ library-consumer/src/main/java/org/projet/library/consumer/impl/ReservationDAOImpl.java
@@ -0,0 +1,61 @@
+package org.projet.library.consumer.impl;
+
+import java.util.List;
+
+import org.projet.library.consumer.contract.ReservationDAO;
+import org.projet.library.model.reservations.Reservation;
+import org.projet.library.model.reservations.ReservationWS;
+import org.projet.library.model.reservations.ReservationWS_Service;
+
+public class ReservationDAOImpl implements ReservationDAO {
+	
+	public List<Reservation> getListReservation() {
+
+		ReservationWS stub = new ReservationWS_Service().getReservationWSPort();
+		
+		List<Reservation> reservation = stub.getListReservation();
+		
+		return reservation;
+	}
+
+	public Reservation getReservation(int id) {
+
+		ReservationWS stub = new ReservationWS_Service().getReservationWSPort();
+		
+		Reservation reservation = stub.getReservation(id);
+		
+		return reservation;
+	}
+
+	public List<Reservation> getPosition(int position) {
+
+		ReservationWS stub = new ReservationWS_Service().getReservationWSPort();
+		
+		List<Reservation> reservation = stub.getListReservation();
+		
+		return reservation;
+	}
+
+	public void deleteReservation(Reservation reservation) {
+
+		ReservationWS stub = new ReservationWS_Service().getReservationWSPort();
+		
+		stub.deleteReservation(reservation);		
+	}
+
+	public void insertReservation(Reservation reservation) {
+		
+		ReservationWS stub = new ReservationWS_Service().getReservationWSPort();
+		
+		stub.insertReservation(reservation);		
+	}
+
+	public List<Reservation> listReservationByUserId(int id) {
+		
+		ReservationWS stub = new ReservationWS_Service().getReservationWSPort();
+		
+		List<Reservation> reservation = stub.listReservationByUserId(id);
+		
+		return reservation;
+	}
+}
