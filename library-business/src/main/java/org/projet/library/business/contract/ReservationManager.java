5a64ff6c5a32b33c94cde2a17188d12638a98c0c
lopotono <30410435+lopotono@users.noreply.github.com>
2019-05-01 18:39:03 +0200

    Update ReservationManager.java

--- library-business/src/main/java/org/projet/library/business/contract/ReservationManager.java
+++ library-business/src/main/java/org/projet/library/business/contract/ReservationManager.java
@@ -5,17 +5,18 @@ import java.util.List;
 import org.projet.library.model.reservations.Reservation;
 
 public interface ReservationManager {
-		
+
 	List<Reservation> listReservationByUserId(int id);
-		
+
 	List<Reservation> getListReservation();
-	
+
 	Reservation getReservation(int id);
-	
-	List<Reservation> getPosition(int position);
-	
+
 	void deleteReservation(Reservation reservation);
-	
+
 	void insertReservation(Reservation reservation);
 
+	List<Reservation> getReservationsByIdLivre(int id);
+
+	List<Reservation> getReservationByIdLivreAndPosition(int id, int position);
 }
