a468e44244a8e91d14cecc8f85f6c5fbd00bb7ac
lopotono <30410435+lopotono@users.noreply.github.com>
2019-05-01 18:12:24 +0200

    Update PretManager.java

--- libraryservice-business/src/main/java/org/projet/libraryservice/business/contract/PretManager.java
+++ libraryservice-business/src/main/java/org/projet/libraryservice/business/contract/PretManager.java
@@ -2,6 +2,7 @@ package org.projet.libraryservice.business.contract;
 
 import java.util.List;
 
+import org.projet.libraryservice.model.Livre;
 import org.projet.libraryservice.model.Pret;
 import org.projet.libraryservice.model.User;
 
@@ -14,6 +15,12 @@ public interface PretManager {
 	void update(Pret pret);
 
 	Pret getPretById(int id);
-	
+
 	List<Pret> getPretLate();
+	
+	List<Pret> getPretOver();
+	
+	List<Pret> getPretByLivre(Livre livre);
+	
+	List<Pret> listPretByUserId(int id);
 }
