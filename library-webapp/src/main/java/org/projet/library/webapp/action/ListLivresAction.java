3a7f2490a365c4172e913395eee87df89e7fb880
lopotono <30410435+lopotono@users.noreply.github.com>
2019-05-01 18:24:30 +0200

    Update ListLivresAction.java

--- library-webapp/src/main/java/org/projet/library/webapp/action/ListLivresAction.java
+++ library-webapp/src/main/java/org/projet/library/webapp/action/ListLivresAction.java
@@ -2,7 +2,7 @@ package org.projet.library.webapp.action;
 
 import java.util.List;
 
-import org.projet.library.model.livres.Livre;
+import org.projet.library.model.livre.Livre;
 
 import com.opensymphony.xwork2.ActionSupport;
 
@@ -14,6 +14,7 @@ public class ListLivresAction extends AbstractAction {
 	private static final long serialVersionUID = -2116271561692587943L;
 	
 	private List<Livre> listLivres;
+	private int id;
 		
 	public List<Livre> getListLivres() {
 		return listLivres;
@@ -23,8 +24,16 @@ public class ListLivresAction extends AbstractAction {
 		this.listLivres = listLivres;
 	}
 	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+		
 	public String doList() {
 		listLivres = getManagerFactory().getLivreManager().getLivres();
 		return ActionSupport.SUCCESS;		
-	}
+	}	
 }
