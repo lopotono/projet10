f380aae2c84a93b12cf3e390e7ff6fed48f02fb5
lopotono <30410435+lopotono@users.noreply.github.com>
2019-05-01 18:38:29 +0200

    Update PretManager.java

--- library-business/src/main/java/org/projet/library/business/contract/PretManager.java
+++ library-business/src/main/java/org/projet/library/business/contract/PretManager.java
@@ -1,9 +1,12 @@
 package org.projet.library.business.contract;
 
+import java.util.Calendar;
 import java.util.List;
 
+import org.projet.library.model.livre.Livre;
 import org.projet.library.model.prets.Pret;
 import org.projet.library.model.prets.User;
+
 
 public interface PretManager {
 	
@@ -16,4 +19,12 @@ public interface PretManager {
 	Pret getPretById(int id);
 	
 	List<Pret> getPretLate();
+	
+	List<Pret> getPretOver();
+
+	Pret getListPret(Calendar datefin);
+	
+	List<Pret> getPretByLivre(Livre livre);
+
+	List<Pret> listPretByUserId(int id);
 }
