991b427db25dae6b490939e813b04ed32a130a75
lopotono <30410435+lopotono@users.noreply.github.com>
2019-05-17 17:18:14 +0200

    Update MailSender.java

--- library-batch/src/main/java/org/projet/library/batch/MailSender.java
+++ library-batch/src/main/java/org/projet/library/batch/MailSender.java
@@ -1,15 +1,19 @@
 package org.projet.library.batch;
 
+import java.util.Properties;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.stereotype.Component;
 
 @Component("projetMailSender")
 public class MailSender {
 
-	@Autowired(required = true)
-	private JavaMailSender javaMailSender;
+	/*@Autowired(required = true)
+	private JavaMailSender javaMailSender;*/
 
 	public void sendMail(String from, String to, String subject, String body) {
 
@@ -22,17 +26,28 @@ public class MailSender {
 
 		System.out.println("Sending");
 
-		javaMailSender.send(mail);
+		getJavaMailSender().send(mail);
 
 		System.out.println("Done !");
 
 	}
 
+	@Bean
 	public JavaMailSender getJavaMailSender() {
-		return javaMailSender;
+		JavaMailSenderImpl jms = new JavaMailSenderImpl();
+		jms.setHost("smtp.gmail.com");
+		jms.setPort(587);
+		jms.setUsername("terragef@gmail.com");
+		jms.setPassword("Innelec_27");
+		
+		Properties p = jms.getJavaMailProperties();
+		p.put("mail.smtp.auth", "true");
+		p.put("mail.smtp.starttls.enable", "true");
+		p.put("mail.transport.protocol", "smtp");
+		return jms;
 	}
 
-	public void setJavaMailSender(JavaMailSender javaMailSender) {
+	/*public void setJavaMailSender(JavaMailSender javaMailSender) {
 		this.javaMailSender = javaMailSender;
-	}
+	}*/
 }
